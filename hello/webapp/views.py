dd0fe3bba48716db73e3aa8e189208c7ae4e3f73
Kunduz <kuzanai@mail.ru>
2021-03-04 19:38:17 +0600

    Создала базу проекта

---
+++ hello/webapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+def main_page(request):
+    if request method==GET:
+        return render(request, base.html)
+
+def good_more():
+    pass
+
+def good_add():
+    pass
+
+def good_change():
+    pass
+
+def good_delete():
+    pass
