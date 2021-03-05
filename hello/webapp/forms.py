e1643893c775cd47adc4c1646a54e4d9163ef900
Kunduz <kuzanai@mail.ru>
2021-03-05 17:46:25 +0600

    Написала функции и шаблоны для просмотра всех товаров, и подробного просмотра товара

--- hello/webapp/forms.py
+++ hello/webapp/forms.py
@@ -1,12 +1,13 @@
+from django import forms
 from webapp.models import Good
 
 
 class Goodform(forms.ModelForm):
     class Meta:
         model=Good
-        feelds=('name', 'description', 'category', 'remainder', 'price')
+        fields=('name', 'description', 'category', 'remainder', 'price')
 
-class GoodDeleteForm(forms.form):
-    name=forms.CharField(maxlength=100, required=True, label='Введите название товара чтобы удалить его')
+class GoodDeleteForm(forms.Form):
+    name=forms.CharField(max_length=100, required=True, label='Введите название товара чтобы удалить его')
 
 
--- hello/webapp/views.py
+++ hello/webapp/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
+from webapp.models import Good
+from webapp.forms import Goodform
+from webapp.forms import Goodform, GoodDeleteForm
+
 # Create your views here.
 def main_page(request):
-    good=get_object_or_404()
-    return render(request, "base.html", {'good':good})
+    good=Good.objects.all()
+    return render(request, "base.html", {'good': good})
+
+
 
 def good_more(request, pk):
     good = get_object_or_404(Good, id=pk)
-    return render(request, "good_more.html", {'good':good})
+    return render(request, "good_more.html", {'good': good})
 
 
 def good_add():
